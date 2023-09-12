f87a3077127294d2e3547973db432c357023fb8a
Greg Johnston <gregmj.sco@gmail.com>
2023-09-12 20:29:09 +0900

    Extract required data into object

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,18 +1,36 @@
 const API_KEY = '734b9c9d87924d27b9590108230309';
 
 const fetchWeatherData = () => {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=london`, {mode: 'cors'})
-        .then((response) =>
-            // successful responce
-             response.json()
-        )
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=london`, { mode: 'cors' })
         .then((response) => {
-            console.log(response)
+            // Check if the response status is OK (status code 200)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Parse the JSON response and extract the relevant properties
+            return response.json().then((data) => {
+                const { location, current } = data;
+                const { name } = location;
+                const { temp_c, feelslike_c } = current;
+
+                // Create a new object with the extracted properties
+                const weatherInfo = {
+                    location: name,
+                    temperature: temp_c,
+                    realFeel: feelslike_c,
+                };
+
+                return weatherInfo;
+            });
+        })
+        .then((weatherInfo) => {
+            // Successfully fetched and extracted data
+            console.log(weatherInfo);
         })
         .catch((err) => {
-            // Error
-            console.log(err);
+            // Handle errors
+            console.error(err);
         });
-}
+};
 
 export default fetchWeatherData;
