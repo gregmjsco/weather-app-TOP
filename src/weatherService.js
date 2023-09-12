b7d2bdab26a6bb4dfe7693336b6992a9be008e39
Greg Johnston <gregmj.sco@gmail.com>
2023-09-12 17:08:28 +0900

    Process data into JSON to retreive required data

--- src/weatherService.js
+++ src/weatherService.js
@@ -2,10 +2,13 @@ const API_KEY = '734b9c9d87924d27b9590108230309';
 
 const fetchWeatherData = () => {
     fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=london`, {mode: 'cors'})
-        .then((response) =>{
+        .then((response) =>
             // successful responce
-            console.log(response.json());
-        })  
+             response.json()
+        )
+        .then((response) => {
+            console.log(response)
+        })
         .catch((err) => {
             // Error
             console.log(err);
